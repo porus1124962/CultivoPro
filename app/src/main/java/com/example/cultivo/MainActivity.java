1f6f19cce0d6f5f4ccdc1befc5572b1e20362274
porus1124962 <itsshafqat.aptech123@gmail.com>
2021-05-02 14:32:02 +0500

    working on signup

--- app/src/main/java/com/example/cultivo/MainActivity.java
+++ app/src/main/java/com/example/cultivo/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.cultivo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    EditText etEmail, etPass, etPhone;
+    EditText etEmail, etPass, etPhone,etName,etWarehouse;
     Button btnRegister, btnGoogle , btnFacebook;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
     ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +38,32 @@ public class MainActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.EtEmail);
         etPass = findViewById(R.id.EtPass);
         etPhone = findViewById(R.id.EtPhone);
+        etName = findViewById(R.id.etName);
+        etWarehouse = findViewById(R.id.wareHouse);
         btnRegister = findViewById(R.id.BtnSignUp);
 
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
-
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), SignIn.class));
+            finish();
+        }
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = etName.getText().toString().trim();
                 String email = etEmail.getText().toString().trim();
                 String pass = etPass.getText().toString().trim();
+                String phone = etPhone.getText().toString().trim();
+                String address = etWarehouse.getText().toString().trim();
 
+                if (TextUtils.isEmpty(name)){
+                    etName.setError("Email is Required");
+                    return;
+                }
                 if (TextUtils.isEmpty(email)){
                     etEmail.setError("Email is Required");
                     return;
@@ -44,17 +72,68 @@ public class MainActivity extends AppCompatActivity {
                     etPass.setError("Pass is Required");
                     return;
                 }
-
                 if (pass.length() < 6){
                     etPass.setError("Pass is Required");
                     return;
                 }
+                if (TextUtils.isEmpty(phone)){
+                    etPhone.setError("Email is Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(address)){
+                    etWarehouse.setError("Email is Required");
+                    return;
+                }
+
 
                 progressBar.setVisibility(View.VISIBLE);
+
+                // register the user in firebase
+
+              /*  fAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), SignIn.class));
+                            finish();
+                        }else{
+                            Toast.makeText(MainActivity.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });*/
+                fAuth.createUserWithEmailAndPassword(email,pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        FirebaseUser user = fAuth.getCurrentUser();
+                        Toast.makeText(MainActivity.this,"Account Created", Toast.LENGTH_SHORT).show();
+                        DocumentReference df = fStore.collection("User").document(user.getUid());
+                        Map<String,Object> userInfo = new HashMap<>();
+                        userInfo.put("Name",name);
+                        userInfo.put("Email",email);
+                        userInfo.put("Password",pass);
+                        userInfo.put("Phone",phone);
+                        userInfo.put("Address",address);
+
+                        //specify if the user is admin
+                        userInfo.put("isUser","1");
+                        df.set(userInfo);
+                        startActivity(new Intent(getApplicationContext(), SignIn.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,"failed to Created", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
 
 
 
             }
+
         });
     }
 }
--- app/src/main/java/com/example/cultivo/SignIn.java
+++ app/src/main/java/com/example/cultivo/SignIn.java
@@ -3,12 +3,49 @@ package com.example.cultivo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class SignIn extends AppCompatActivity {
-
+    EditText email,password;
+    TextView gotoRegister;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        email = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPass);
+        login = findViewById(R.id.btnLogin);
+        gotoRegister = findViewById(R.id.gotoRegister);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailText = email.getText().toString().trim();
+                String passwordText = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(emailText)){
+                    email.setError("Email is Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(passwordText)){
+                    password.setError("Email is Required");
+                    return;
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
